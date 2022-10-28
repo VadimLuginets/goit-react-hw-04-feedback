84a6aa83823fbb06a0834301fbd941cc09a84b70
Vadim luginets <pebeeo@gmail.com>
2022-10-28 15:47:53 +0300

    works
    recode app from class to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,53 @@
-import React from 'react';
+import { useState } from 'react';
 import Section from 'components/Section/Section';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Statistics from 'components/Statistics/Statistics';
 import Message from 'components/Message/Message';
 
-export default class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const array = { good: good, neutral: neutral, bad: bad };
+  const total = bad + neutral + good;
+  const positivePercentage = (good / total) * 100;
+  const onLeaveFeedback = event => {
+    switch (event) {
+      case 'good':
+        setGood(prev => prev + 1);
+        break;
+      case 'neutral':
+        setNeutral(prev => prev + 1);
+        break;
+      case 'bad':
+        setBad(prev => prev + 1);
+        break;
+      default:
+        break;
+    }
   };
-
-  handlClickFeedback = key => {
-    this.setState(prevState => {
-      return {
-        [key]: prevState[key] + 1,
-      };
-    });
-  };
-
-  render() {
-    const total = this.state.bad + this.state.neutral + this.state.good;
-    const positivePercentage = (this.state.good / total) * 100;
-    return (
-      <div>
-        <Section
-          title="Plese Leave feedback"
-          children={
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.handlClickFeedback}
-            />
-          }
-        />
-        {total === 0 ? (
-          <Message message={'There is no feedback'} />
-        ) : (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={total}
-            positivePercentage={Math.round(positivePercentage)}
+  return (
+    <div>
+      <Section
+        title="Plese Leave feedback"
+        children={
+          <FeedbackOptions
+            options={Object.keys(array)}
+            onLeaveFeedback={onLeaveFeedback}
           />
-        )}
-      </div>
-    );
-  }
+        }
+      />
+      {total === 0 ? (
+        <Message message={'There is no feedback'} />
+      ) : (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={Math.round(positivePercentage)}
+        />
+      )}
+    </div>
+  );
 }
